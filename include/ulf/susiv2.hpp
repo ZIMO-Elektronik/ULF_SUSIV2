eb9d0061e6dc3f2e8be5616e8fccb52b516cbf62
Vincent Hamp <vincent.hamp@higaski.at>
2024-10-14 18:51:02 +0200

    feat: add ack/nak definitions

---
+++ include/ulf/susiv2.hpp
@@ -0,0 +1,14 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+/// ULF_SUSIV2
+///
+/// \file   ulf/susiv2.hpp
+/// \author Vincent Hamp
+/// \date   18/08/2024
+
+#pragma once
+
+#include "susiv2/ack.hpp"
+#include "susiv2/nak.hpp"
---
+++ tests/test.cpp
@@ -0,0 +1,3 @@
+#include <gtest/gtest.h>
+
+TEST(test, replace_me) { EXPECT_TRUE(true); }
